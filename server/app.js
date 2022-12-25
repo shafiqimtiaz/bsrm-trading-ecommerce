c97f9e40b84138c676ab162025dfe60785f61984
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2022-12-25 14:36:26 -0500

    setup up filter

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 require("express-async-errors");
-const express = require("express");
 const app = express();
-const cors = require("cors");
-const morgan = require("morgan");
-const error = require("./middlewares/error");
 
 const userRouter = require("./routers/userRouter");
 const categoryRouter = require("./routers/categoryRouter");
+const productRouter = require("./routers/productRouter");
 
 app.use(express.json());
 app.use(cors());
@@ -17,6 +14,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/user", userRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
 
 app.use(error);
 
---
+++ server/middlewares/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const error = require("./middlewares/error");
