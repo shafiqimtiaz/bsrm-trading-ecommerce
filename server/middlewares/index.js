b6f967e86c675c5fafed9c931c480e9c30f8139f
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2022-12-25 17:13:38 -0500

    refactor app.js

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const error = require("./middlewares/error");
+
+module.exports = (app) => {
+	app.use(express.json());
+	app.use(cors());
+
+	if (process.env.NODE_ENV === "development") {
+		app.use(morgan("dev"));
+	}
+};
