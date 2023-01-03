26b4e883aa158bd16b9165513f84d688eab0b906
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-01-02 20:11:06 -0500

    refactor app and server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 require("express-async-errors");
 const express = require("express");
 const compression = require("compression");
-const app = express();
+const cors = require("cors");
 const error = require("./middlewares/error");
+const morgan = require("morgan");
 
-require("./middlewares")(app);
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
 require("./middlewares/routes")(app);
 app.use(error);
 app.use(compression);
